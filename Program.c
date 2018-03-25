788f36b1dec2b16400113b4866b52a4eb5f7f99a
Sahil Sinha <Sahil.11612944@lpu.in>
2018-03-25 12:51:40 +0530

    Added jobMerger() and summary()

--- Program.c
+++ Program.c
@@ -7,39 +7,73 @@ struct jobDetails{
 	int complete;
 	int turnAroundTime;
 	int waitTime;
-}student[100], faculty[100];
+}student[100], faculty[100], ready[100];
 
-void enqueue(int a[], int rear);
-void dequeue(int a[], int front);
+int facultyJobCount=0;
+int studentJobCount=0;
+int rpointer=0;
 
-void getStats(int findex, int sindex);
-
-void Scheduler();
-
-int timeQuanta= 2;
-
-void Scheduler(){
-	int fpointer=0, spointer=0;
-	int time= 120, i;
-	for(i=0; i<=time; i++){
-		if(student[spointer].arrivalTime == i || faculty[fpointer] == i){
-			if(faculty[fpointer]== i){
-				
+void readyGen(){
+	int i;
+	int s=0, f=0;
+	int r=0;
+	for(i=0; i<=120; i++){
+		if(i== student[s].arrivalTime && i == faculty[f].arrivalTime){
+			ready[r] = faculty[f]; r++; f++;
+			while(i== faculty[f].arrivalTime){
+				ready[r]= faculty[f]; r++; f++;
 			}
-			else{
-				
+			ready[r]= student[s]; r++; s++;	
+			while(i== student[s].arrivalTime){
+				ready[r]= student[s]; r++; s++; 
+			}		
+		}
+		else if(i == student[s].arrivalTime){
+			ready[r] = student[s];
+			r++;
+			s++; 
+			while(i== student[s].arrivalTime){
+				ready[r]= student[s];
+				r++; s++;
 			}
-		}	
-		else{
-			continue;
-		}	
+		}
+		else if(i == faculty[f].arrivalTime){
+			ready[r]= faculty[f];
+			r++;
+			f++; 
+			while(i == faculty[f].arrivalTime){
+				ready[r]= faculty[f];
+				r++; f++;
+			}
+		}
+		else continue; 
+	} 
+	rpointer= r;
+}
+
+void printer(){
+	int i=0, j=0;
+	printf("\nPrinter\n");
+	printf("Faculty Jobs: \n");
+	for(i=0; i<facultyJobCount; i++){
+		printf("Id: %d, Atime: %d, Btime: %d\n", faculty[i].processId, faculty[i].arrivalTime, faculty[i].burstTime);
+	}
+	printf("Student Jobs: \n");
+	for(i=0; i<studentJobCount; i++){
+		printf("Id: %d, Atime: %d, Btime: %d\n", student[i].processId, student[i].arrivalTime, student[i].burstTime);
+	}
+	printf("\nReady Jobs: \n");
+	for(i=0; i<rpointer; i++){
+		printf("Id: %d, Atime: %d, Btime: %d\n", ready[i].processId, ready[i].arrivalTime, ready[i].burstTime);
 	}
 }
 
+
+void summary(int findex, int sindex);
+
 void main(){
 	int total_query, map, i;
-	int facultyJobCount=0, studentJobCount=0;
-	printf("Welcome, Mr. Sudhesh\n");
+	printf("Welcome\n");
 	printf("\nEnter the total number of process to handle: ");
 	scanf("%d", &total_query);
 	for(i=0; i<total_query; i++){
@@ -62,12 +96,14 @@ void main(){
 			studentJobCount++;
 		}
 	}
-	getStats(facultyJobCount, studentJobCount);
+	readyGen();
+	printer();
+	summary(facultyJobCount, studentJobCount);
 }
 
-void getStats(int findex, int sindex){
+void summary(int findex, int sindex){
 	int i;
-	printf("\nDeatails of all Jobs Done:");
+	printf("\nSummary:");
 	printf("\nFaculty's QueriesDetails\n");
 	printf("\nQuery Id \t Arrival Time \t Resolving Time\n");
 	for (i = 0; i < findex; i++) {
