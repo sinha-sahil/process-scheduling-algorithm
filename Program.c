580f45d0fa5ee210b2b4719d67906430b713ac22
Sahil Sinha <Sahil.11612944@lpu.in>
2018-04-04 03:20:10 +0530

    roundRobin() added

--- Program.c
+++ Program.c
@@ -4,10 +4,31 @@ struct job{
 	int pid;
 	int at;
 	int bt;
-	int tat; int wt;
+	int tat;
+	int wt;
+	int rbt;
 }f[100], s[100], m[100];
 
 int n, fc=0, sc=0, mc=0;
+int quanta;
+
+void roundRobin(){
+	int time=0,i, cCount=0;
+	while(1){
+		for(i=0; i<mc; i++){
+			if(m[i].rbt>quanta){
+				time += quanta; m[i].rbt -= quanta;
+			}
+			else if(m[i].rbt ==0){ continue;
+			}
+			else{
+				time += m[i].rbt; m[i].rbt=0; cCount++;
+			}
+		}
+		if(time==120 || cCount == mc) break;
+	}
+	
+}
 
 void merger(){
 	int min= fc, i, flag=1, r=0, maxr=0;
@@ -69,7 +90,7 @@ void merger(){
 void printer(){
 	int i=0; 
 	for(i; i<mc; i++){
-		printf("\n\nId: %d Atime: %d", m[i].pid, m[i].at);
+		printf("\n\nId: %d Atime: %d, RBT: %d", m[i].pid, m[i].at, m[i].rbt);
 	}
 }
 
@@ -78,18 +99,19 @@ void input(){
 	printf("Enter total no of queries: "); scanf("%d", &n);
 	if(n==0) { printf("\n No queries\n"); exit(0);}
 	else{
+		printf("\nEnter Quanta for each Process: "); scanf("%d", &quanta);
 		printf("\nEnter 1 for faculty and 2 for student\n");
 		for(i=0; i<n; i++){
 			printf("\nJob Type (1/2): "); scanf("%d", &map);
 			if(map==1){
 				printf("Query Id: "); scanf("%d", &f[fc].pid);
 				printf("Arrival Time: "); scanf("%d", &t); f[fc].at= t-1000;
-				printf("Resolving Time: "); scanf("%d", &f[fc].bt);
+				printf("Resolving Time: "); scanf("%d", &f[fc].bt); f[fc].rbt= f[fc].bt; 
 				fc++;
 			} else{
 				printf("Query Id: "); scanf("%d", &s[sc].pid);
 				printf("Arrival Time: "); scanf("%d", &t); s[sc].at= t-1000;
-				printf("Resolving Time: "); scanf("%d", &s[sc].bt);	
+				printf("Resolving Time: "); scanf("%d", &s[sc].bt);	 s[sc].rbt= s[sc].bt;
 				sc++;
 			}
 		}
@@ -99,5 +121,6 @@ void input(){
 void main(){
 	input();
 	merger();
+	roundRobin();
 	printer();
 }
